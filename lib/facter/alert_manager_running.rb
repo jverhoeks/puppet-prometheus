3423d9571a07189117be34ea329137ab5f63b964
Jacob <jacob.verhoeks@gmail.com>
2018-01-15 15:24:18 +0100

    Service is without _

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -1,6 +1,6 @@
 require 'puppet'
 
-service = Puppet::Type.type(:service).new(name: 'alert_manager')
+service = Puppet::Type.type(:service).new(name: 'alertmanager')
 
 Facter.add('prometheus_alert_manager_running') do
   setcode do
